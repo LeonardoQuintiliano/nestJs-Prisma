9523861837e492581f8f69c08e20e6f7271ff93c
LeonardoQuintiliano <leoqds18@gmail.com>
2023-04-03 00:18:44 -0300

    Adicionando book dto e prisma service

---
+++ src/modules/book/book.dto.ts
@@ -0,0 +1,7 @@
+export type BookDTO = {
+    id? : String,
+    name: String,
+    description: String,
+    author: String,
+    bar_code: String
+}
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { BookDTO } from './book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    async create(data: BookDTO) {
+        
+    }
+}
