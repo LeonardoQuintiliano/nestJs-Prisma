543293a5cb295f3f318ded8bc9ca97b0e8990a09
Leonardo Quintiliano <leoks141@hotmail.com>
2025-12-08 02:29:59 -0300

    feat: implementing user controller

---
+++ src/modules/user/implementations/user.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { CreateUserDto } from "../dto/createUser.dto";
+import { UserDto } from "../dto/user.dto";
+import { IUserController } from "../interfaces/user.controller.interface";
+import { UserService } from "./user.service";
+
+export class UserController implements IUserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    async create(@Body() data: CreateUserDto): Promise<UserDto> {
+        return this.userService.create(data);
+    }
+
+    @Get()
+    async findAll(): Promise<UserDto[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param() id: string): Promise<UserDto> {
+        return this.userService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param() id: string, @Body() data: CreateUserDto) {
+        return this.userService.update(id, data);
+    }
+
+    @Delete(':id')
+    async delete(@Param() id: string): Promise<string> {
+        return this.userService.delete(id);
+    }
+    
+}
