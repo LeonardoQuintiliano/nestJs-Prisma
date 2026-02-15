ee2ea83cfa354dee5382bc67b00cefcef41465ff
Leonardo Quintiliano <leoks141@hotmail.com>
2026-02-15 02:08:36 -0300

    feat: implementing the import data endpoint

--- src/modules/book/implementations/external.book.service.ts
+++ src/modules/book/implementations/external.book.service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { IExternalBookService } from "../interfaces/external.book.service.interface";
 import { PrismaService } from "src/database/PrismaService";
 import { Injectable } from "@nestjs/common";
+import { connect } from "http2";
 
 @Injectable()
 export class ExternalBookService implements IExternalBookService {
@@ -13,13 +14,16 @@ export class ExternalBookService implements IExternalBookService {
                 `${baseUrl}/${isbn}.json`
             );
 
-            console.log("Data received by openlibrary: ", response);
+            console.log("Data received by openlibrary: ", response.data);
 
-            const bookData = this.mapOpenLibraryToBookModel(response, userId);
+            const bookData = this.mapOpenLibraryToBookModel(response.data, userId);
             console.log("Body formatted to prisma: ", bookData);
 
             return await this.prisma.book.create({
-                data: bookData
+                data: {
+                    ...bookData,
+                    ownerId: userId
+                }
             });
         } catch (error) {
             throw new Error(error);
@@ -28,10 +32,10 @@ export class ExternalBookService implements IExternalBookService {
 
     private mapOpenLibraryToBookModel(data: any, userId: string) {
         return {
-            name: data.name,
-            description: data.description,
-            author: data.author,
-            bar_code: data.bar_code,
+            name: data.title,
+            description: `Published in ${data.publish_date} by ${data.publishers[0]}`,
+            author: data.author || "unknown",
+            bar_code: data.isbn_10[0],
             ownerId: userId,
         };
     }
